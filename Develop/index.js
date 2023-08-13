e7be9b335fe2fea8e05d789333d602f0aca9b943
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-08-13 17:28:17 -0600

    added notes to index.js, added ds_store to gitignore

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,14 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = ["Generate README.md file?",  ]
+//["What is your project title?", "What is the description of your project?", "What do you want inside the Table of Contents?", "Please type your installation instructions", "Please type what the usage of this project is", "What kind of license would you like?", "What are YOUR guidelines on contributions to this application", "What tests can a user run when troubleshooting this application?"];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
+//if userAnswers === questions[0] return writeToFile()
 
 // Function call to initialize app
 init();
